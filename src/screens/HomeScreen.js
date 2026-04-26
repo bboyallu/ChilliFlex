7a38cbfd9f84745f0aa862761c94c4e19d6ba247
DegonPrime <50234668+bboyallu@users.noreply.github.com>
2026-04-26 17:20:02 +0300

    Add HomeScreen component with bug documentation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+// HomeScreen.js
+import React from 'react';
+
+const HomeScreen = () => {
+    // Bug: Media items not rendering properly; check media fetching logic.
+    return (
+        <div>
+            <h1>Media Items</h1>
+            {/* Display media items here */}
+        </div>
+    );
+};
+
+export default HomeScreen;
